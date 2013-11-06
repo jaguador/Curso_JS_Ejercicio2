fc22c204322906c7b084a3b50430256546a233da
jaguador <jaguador@gmail.com>
2013-11-06 14:24:31 +0100

    Terminado las fichas en el tablero
    Pendiente la comprobación de partida ganada y el color del turno que
    toca

--- ejercicio2.js
+++ ejercicio2.js
@@ -5,15 +5,18 @@
 ********************/
 
 var tablero;
+var turnos = ["rojo", "azul"];
 
 // Clase Tablero
 function Tablero(filas, columnas) {
 	this.tablero = null;		// array del tablero
 	this.filas = filas;
 	this.columnas = columnas;
-	this.turno = null;	// Turno actual 
+	this.turno = turnos[0];	// Turno actual (siempre comienza rojo)
+	this.jugadas = 0;
 	this.literalPuntuacion = null;	// Literal de la puntuacion
 	this.dibujaTablero = dibujaTablero;
+	this.chkGanador = chkGanador;
 	
 	// Inicializar el array a vacio
 	this.tablero = new Array(this.filas);
@@ -24,18 +27,50 @@ function Tablero(filas, columnas) {
 	}				
 }
 
+function chkGanador(posicion_fila, posicion_columna, color) {
+	var contador, pos_col, pos_fil;
+	// Mirar a la derecha
+	contador = 1;
+	pos_col = posicion_columna;
+	while (pos_col < this.columnas && this.tablero[posicion_fila][pos_col] == color ) {
+		contador++;
+		pos_col++;
+	}
+	if (contador == 4) alert('encontrado horizontal a la derecha');
+	// Mirar a la izquierda
+	contador = 1;
+	pos_col = posicion_columna;
+	while (pos_col >= 0 && this.tablero[posicion_fila][pos_col] == color ) {
+		contador++;
+		pos_col--;
+	}
+	if (contador == 4) alert('encontrado horizontal a la izquierda');
+	// Mirar abajo
+	contador = 1;
+	pos_fil = posicion_fila
+	while (pos_fil >= this.filas && this.tablero[pos_fil][posicion_columna] == color ) {
+		contador++;
+		pos_fil++;
+	}
+	if (contador == 4) alert('encontrado vertical hacia abajo');
+	
+}
+
+
+
 function dibujaTablero() {
 	// Crear la tabla con el tablero de juego
 	var html = "<table id='tblTablero' cellpadding=0 cellspacing=0><tr>";
 	// Botones para colocar ficha
 	for (var i=0; i<this.columnas; i++)
 		html += "<td><input type='button' value='Ficha' id='btnCol_"+i+"'><script type=\"text/javascript\">$(\"#btnCol_"+i+"\").click(colocarFicha);</script></td>";
-	html += "</tr>";
+	html += "<td id='celda_blanco'><div>&nbsp;</div></td></tr>";
 	for (var i=0; i<this.filas; i++) {
 		html += "<tr>";
 		for (var j=0; j<this.columnas; j++)
-			html+= "<td id='celda_"+i+"_"+j+"' class='casilla'><div id='div_"+i+"_"+j+"' class='color_rojo'>a</div></td>";
-		html += "<tr>";
+//			html+= "<td id='celda_"+i+"_"+j+"' class='casilla'><div class='wrapper'><div class='blanco'>&nbsp;</div><div id='div_"+i+"_"+j+"' >&nbsp;</div></div></td>";
+			html+= "<td id='celda_"+i+"_"+j+"' class='casilla'><div id='div_"+i+"_"+j+"' >&nbsp;</div></td>";
+		html += "<td id='celda_blanco'><div>&nbsp;</div></td><tr>";
 	}
 	html+= "</tr>";
 	html+= "</table>";
@@ -45,9 +80,24 @@ function dibujaTablero() {
 
 
 function colocarFicha() {
+	var velocidad = 200;
 	var columna = this.id.split('_')[1];
-	for (var i=0; i<tablero.filas; i++) {
-		$("#div_"+i+"_"+columna).fadeOut( 300 );
+	var i = 0;
+	while (i<(tablero.filas) && tablero.tablero[i][columna] == "") {
+		$("#div_"+i+"_"+columna).hide();
+		$("#div_"+i+"_"+columna).addClass(tablero.turno);		
+		if (i<(tablero.filas-1) && tablero.tablero[i+1][columna] == "") {
+			$("#div_"+i+"_"+columna).delay(velocidad*i).fadeIn(velocidad).fadeOut(0);
+		} 
+		else
+			$("#div_"+i+"_"+columna).delay(velocidad*i).fadeIn(velocidad);
+		i++;
+	}
+	if (i > 0) {
+		tablero.tablero[i-1][columna] = tablero.turno;
+		tablero.jugadas++;
+		tablero.chkGanador(i-1, parseInt(columna), tablero.turno);
+		tablero.turno = turnos[tablero.jugadas%turnos.length];
 	}
 } 
 
@@ -63,49 +113,4 @@ function iniciaJuego() {
 		alert('Debe introducir valores numéricos para filas y columnas');
 }
 
-// Funcion principal llamada desde el origen HTML
-function mostrarResultado(valoracion, idDivHTML, idDivJSON) {
- // Se crea un Array el listado de webs (separadas por ;)
- var listado = datos.split(';');  
- var webs = new Array;
- var valorEntero = parseInt(valoracion);
- var ficheroHTML = "";
- var ficheroJSON = "";
- var cont = 0;	// Contador de webs leidas
- 
- // Recorrer el array para crear los objetos web
- for (var i in listado) {
-	// Tomar la url y puntuacion separados por #
-	var registro = listado[i].split('#');
-	// Si la valoracion es la que se busca o se indica que se busquen todas
-	if (parseInt(registro[1]) == valorEntero || valorEntero === 0) {
-		webs[cont] = new Web(registro[0], parseInt(registro[1]));
-		webs[cont].setLiteralPuntuacion(parseInt(registro[1]));
-		cont++;
-	}
- }
-
- // Si no se han encontrado con los criterios, se indica
- if (cont == 0) {
-	ficheroHTML = "(No se han encontrado webs con el criterio seleccionado)";
-	ficheroJSON = "[]";
- } else {
-	 // Recorrer las webs generadas para obtener el fichero html
-	 ficheroHTML = "<ul>";
-	 for (var i in webs) {
-		ficheroHTML += "<li><a href='"+webs[i].url+"' target='_blank'>"+webs[i].url+"</a></li>";
-	 }
-	 ficheroHTML += "</ul>";
-	 ficheroJSON = "[";
-	 // Recorrer las webs generadas para obtener el fichero JSON
-	 for (var i in webs) {
-		ficheroJSON += " {url: '"+webs[i].url+"', puntuacion: "+webs[i].puntuacion+", literalPuntuacion: '"+webs[i].literalPuntuacion+"'},";
-	 }
-	 ficheroJSON = ficheroJSON.substring(0, ficheroJSON.length -1);  // Se quita la última ',
-	 ficheroJSON += "]";   // Se cierra el vector de objetos JSON
- }
- // Mostrar el resultado final
- document.getElementById(idDivHTML).innerHTML  = ficheroHTML;
- document.getElementById(idDivJSON).innerHTML  = ficheroJSON; 
-}
 
